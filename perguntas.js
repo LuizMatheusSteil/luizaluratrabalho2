046597399250270d103d75c5717f7dec264a6567
LuizMatheusSteil <146950254+LuizMatheusSteil@users.noreply.github.com>
2024-09-17 08:49:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quanto tempo durou a Primeira Guerra Mundial?',
+    '4 anos, de 1914-1918'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Luxemburgo?',
+    'A capital do Luxemburgo é Luxemburgo.'
+)
+
+criaCartao(
+    'História',
+    'Quem fundou o Islã?',
+    'Maomé, após ter escutado o "evangelho" de um anjo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Andorra?',
+    'A capital Andorra é Andorra-a-Velha'
+)
